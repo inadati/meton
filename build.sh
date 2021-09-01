475198f7edbb5e97fb504e8dabe4611c7602f4d7
Inadati <dr.inadati@gmail.com>
2021-09-01 19:29:06 +0900

    build用のshellを用意

---
+++ build.sh
@@ -0,0 +1,5 @@
+#!/bin/zsh
+
+GOOS=darwin GOARCH=amd64 go build -o meton_darwin-amd64 main.go
+GOOS=linux GOARCH=amd64 go build -o meton_linux-amd64 main.go
+GOOS=windows GOARCH=amd64 go build -o meton_linux-amd64 main.go
